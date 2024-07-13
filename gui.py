1d9f5156b6a4f67a216da8d60a0d7332c1136732
fd <dalirani.1373@gmail.com>
2024-07-13 10:09:03 -0400

    add reconstruction to gui also use caching to load model and data once

--- gui.py
+++ gui.py
@@ -3,37 +3,74 @@ import os
 import json
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import utils
 from variational_autoencoder import VAE
 from utilities import get_split_data, get_random_images
-from synthesis import generate
+from synthesis import generate, reconstruct
 
 def generate_images(config, model_vae):
-    images_list = generate(decoder=model_vae.dec, emd_size=config["embedding_size"], num_generated_imgs=70)
+    """
+    Generate new images with VAE and concatenate images to create one image
+    """
+    images_list = generate(decoder=model_vae.dec, emd_size=config["embedding_size"],
+                            num_generated_imgs=70)
     rows = []
     for i in range(7):
         rows.append(np.concatenate((images_list[(i*10):((i+1)*10)]), axis=1))
     all_images = np.concatenate(rows, axis=0)
     return all_images
 
-def main(config):
+def reconstruct_images(model_vae, validation):
+    """
+    Randomly select some faces from CelebA dataset, feed to VAE to 
+    reconstruct, then concatenate images
+    """
+    # Get some random images from dataset
+    images = get_random_images(dataset=validation, num_images=40)
+    
+    # Reconstruct some images by VAE
+    list_imgs_recons = reconstruct(vae=model_vae, input_images=images)
+    
+    rows = []
+    for i in range(4):
+        row_rec = np.concatenate((list_imgs_recons[(i*10):((i+1)*10)]), axis=1)
+        row_org = np.concatenate(([images[i] for i in range((i*10), ((i+1)*10))]), axis=1)
+        rows.append(np.concatenate((row_org, row_rec), axis=0))
+    all_images = np.concatenate(rows, axis=0)
+    return all_images
+
+def main(config, model_vae, validation):
     st.title("Controllable Face Generation VAE")
     st.sidebar.title("Settings")
     st.sidebar.subheader("Features")
 
     # Select between different features
-    app_feature = st.sidebar.selectbox("Choose the App Mode", ['Generate Faces', 'Face Latent Space Arithmetic', 'Morph Faces'])
+    app_feature = st.sidebar.selectbox("Choose the App Mode", [
+                                        'Generate Faces', 
+                                        'Reconstruct Faces', 
+                                        'Face Latent Space Arithmetic', 
+                                        'Morph Faces'])
 
     if app_feature == 'Generate Faces':
-        st.markdown("Randomly select 10 vectors from a standard normal distribution and feed them to the decoder to generate new faces")
+        st.markdown("Randomly select vectors from a standard normal distribution and feed them to the decoder to generate new faces")
 
-        if 'images' not in st.session_state:
-            st.session_state.images = generate_images(config, model_vae)
+        if 'images_gen' not in st.session_state:
+            st.session_state.images_gen = generate_images(config, model_vae)
 
         if st.button('Generate New Faces'):
-            st.session_state.images = generate_images(config, model_vae)
+            st.session_state.images_gen = generate_images(config, model_vae)
 
-        st.image(st.session_state.images)
+        st.image(st.session_state.images_gen)
+
+    elif app_feature == 'Reconstruct Faces':
+        st.markdown("Randomly select faces from the CelebA dataset, feed them to a variational autoencoder, and depict the reconstructed faces")
+        
+        if 'images_rec' not in st.session_state:
+            st.session_state.images_rec = reconstruct_images(model_vae, validation)
+
+        if st.button('Reconstruct New Faces'):
+            st.session_state.images_rec = reconstruct_images(model_vae, validation)
+
+        st.image(st.session_state.images_rec)
 
     elif app_feature == 'Face Latent Space Arithmetic':
         pass
@@ -48,16 +85,22 @@ if __name__ == '__main__':
     with open(config_path, 'r') as file:
         config = json.load(file)
 
-    # Load VEA model
-    model_vae = VAE(
-        input_img_size=config["input_img_size"], 
-        embedding_size=config["embedding_size"], 
-        num_channels=config["num_channels"], 
-        beta=config["beta"])
-    model_vae.load_weights(os.path.join(config["model_save_path"], "vae.keras"))
-    model_vae.summary()
+    if "model" not in st.session_state.keys():
+        # Load VAE model
+        st.session_state["model"] = VAE(
+            input_img_size=config["input_img_size"], 
+            embedding_size=config["embedding_size"], 
+            num_channels=config["num_channels"], 
+            beta=config["beta"])
+        st.session_state["model"].load_weights(os.path.join(config["model_save_path"], "vae.keras"))
+        st.session_state["model"].summary()
+
+    # Load dataset
+    if "val_data" not in st.session_state.keys():
+        train, st.session_state["val_data"] = get_split_data(config=config)
+    
 
     try:
-        main(config)
+        main(config, st.session_state["model"], st.session_state["val_data"])
     except SystemExit:
         pass
